e159df05c3c3989672398686690ddd2927920871
HARUKI1127 <26urakami@nao.net.it-chiba.ac.jp>
2025-11-21 12:25:29 +0900

    Model：ロジッククラスの作成

--- MyFirstWebApp/src/main/java/model/LoginLogic.java
+++ MyFirstWebApp/src/main/java/model/LoginLogic.java
@@ -2,4 +2,13 @@ package model;
 
 public class LoginLogic {
 
+    public boolean execute(User user) {
+        // 本来はここでデータベースと照合する
+        // 今回は簡易的に、IDとパスワードが一致するかどうかだけをチェック
+        if (user.getPass().equals("1234")) {
+            return true; // ログイン成功
+        }
+        return false; // ログイン失敗
+    }
 }
+
